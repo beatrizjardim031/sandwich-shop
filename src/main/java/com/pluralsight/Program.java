f419da941901cabe65ed00c5d49daa01923a1721
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-10 15:08:12 -0500

    Feat: Add print and scanner lines asking for Sandwich size and user age

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -6,14 +6,18 @@ public class Program {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to Talon's Sandwiches!");
+        System.out.printf("Welcome to Talon's Sandwiches! %n%n");
 
         System.out.println("Choose your sandwich size: ");
+        System.out.println("1: Regular: base price $5.45");
+        System.out.println("2: Large: base price $8.95");
+        System.out.print("Number: ");
         int size = input.nextInt();
-
+        System.out.printf("%n");
 /*  1: Regular: base price $5.45
     2: Large: base price $8.95 */
-        System.out.println("Tell us how old are you: ");
+
+        System.out.printf("Tell us how old are you: ");
         int age = input.nextInt();
 
     }
